e6f37f2412cfa5afaf235056462589dd6d6b8c29
Harvey W. Kis <harveykisiangani@gmail.com>
2024-05-30 17:30:13 +0300

    removed extra empty list

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,17 +14,14 @@ def pascal_triangle(n):
     current_row = [1]
     if (n <= 0):
         return pascal
-    pascal.append(1)
+    pascal.append([1])
     if (n == 1):
-        return pascal
-    if (n == 2):
-        pascal.append(1)
         return pascal
     for index in range(1, n):
         for row in range(1, index):
             c = prev_row[row - 1] + prev_row[row]
             current_row.append(c)
-        current.append(1)
+        current_row.append(1)
         pascal.append(current_row)
         prev_row = current_row
         current_row = [1]
