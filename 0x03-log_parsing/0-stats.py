33366bc051c4ff1899e9de6e75bac0a6def34cc7
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-21 11:58:47 +0300

    check if the file size string is convertible to number

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,31 +20,48 @@ count = 0
 interrupt = False
 file_size = 0
 
+
+def is_number(string: str) -> bool:
+    """Function to check is a string can be converted to a number
+    Args:
+        string: the string to be checked
+    Returns: True if yes, False if it cannot be conveted
+    """
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+
 try:
     for line in sys.stdin:
         striped = line.split(" ")
-        file_size += int(striped[len(striped) - 1])
+        file_s = striped[len(striped) - 1]
+        # print("True or False\t{}, \t {}".format(is_number(file_s), file_s))
+        if is_number(file_s) is True:
+            file_size += int(file_s)
         print("File size: {}".format(file_size))
-        status = striped[7]
+        status = striped[len(striped) - 2]
         for key, value in status_dict.items():
             if status == key:
                 status_dict[key] += 1
         for key in status_dict:
-            check = int(status)
             if status_dict[key] == 0:
                 continue
             print("{}: {}".format(key, status_dict[key]))
+
 except KeyboardInterrupt as e:
-    raise e
-    """print("File size: {}".format(file_size))
+    print("File size: {}".format(file_size))
     for key in status_dict:
         if status_dict[key] == 0:
             continue
-        print("{}: {}".format(key, status_dict[key]))"""
-
+        print("{}: {}".format(key, status_dict[key]))
+"""
 finally:
     print("File size: {}".format(file_size))
     for key in status_dict:
         if status_dict[key] == 0:
             continue
         print("{}: {}".format(key, status_dict[key]))
+"""
