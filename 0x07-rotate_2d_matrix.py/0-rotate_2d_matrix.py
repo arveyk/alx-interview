240ac6428f8b5de078c36261fc6bc0223de9afd4
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-16 11:27:32 +0300

    pycodestyle

--- 0x07-rotate_2d_matrix.py/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix.py/0-rotate_2d_matrix.py
@@ -13,23 +13,11 @@ def rotate_2d_matrix(matrix):
     print(size)
     for x in range(0, int(size / 2)):
         for y in range(x, size - x - 1):
-            """
-            temp = matrix[x][y]
-            temp2 = matrix[size - 1 - x][size - 1 - x]
-            temp3 = matrix[size - 1]
-
-            matrix[x][y] = matrix[y][size - 1 - x]
-            matrix[y][size - 1 - x] = matrix[size - 1 - x][size - 1 - y]
-
-            matrix[size - 1 - x][size - 1 - y] = matrix[size - 1 - y][x] 
-            matrix[size - 1 - y][x] = temp
-            """
             temp = matrix[x][y]
             temp2 = matrix[x][size - 1]
 
-            matrix[x][y]  = matrix[size - y - 1][x]
+            matrix[x][y] = matrix[size - y - 1][x]
             matrix[size - y - 1][x] = matrix[size - x - 1][size - y - 1]
 
             matrix[size - x - 1][size - y - 1] = matrix[y][size - x - 1]
             matrix[y][size - x - 1] = temp
-
