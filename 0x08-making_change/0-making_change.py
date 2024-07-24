7bc9467e8dee92625bc220120b983ab95ba72deb
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-24 16:18:59 +0300

    README.md, Making change complete

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,4 +14,28 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
     coins.sort()
-    return coins
+    listLength = len(coins) - 1
+
+    quo = coins[listLength]
+    div = 0
+    num = total
+    if num % quo == 0:
+        return num / quo
+    Div_list = []
+    while listLength >= 0:
+        div += num // quo
+        rem = num % quo
+        if rem < quo:
+            Div_list.append(num // quo)
+            quo = coins[listLength - 1]
+            num = rem
+            listLength -= 1
+    listLength = len(coins) - 1
+    sum_of_coins = 0
+    for x in Div_list:
+        sum_of_coins += x * coins[listLength]
+        listLength -= 1
+
+    if sum_of_coins != total:
+        return -1
+    return div
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,13 @@
+<h3><-- ALX Interview --></h3>
+<h4>Making Change</h4>
+
+<u>Concepts considered</u>
+<ul>
+<li>-- Greedy Algorithms</li>
+<li>-- Dynamic Programming</li>
+<li>-- Problems-Solving Strategies</li>
+<li>-- Python Programming</li>
+</ul>
+
+The making change challange is: Given a collection of denominations/coins eg 
+[1, 2, 25], what is the least amount of coins needed to make a given amount, say 572?
