8d43e338cff2d1e160b097397d614ae43ac64f85
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-29 14:35:28 +0300

    Island perimeter, Interview quiz, first commit

---
+++ 0x09-island_peremeter/0-island_perimeter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+island_perimeter Function
+"""
+
+
+def island_perimeter(grid):
+    """Evaluates the perimeter of an island described in grid
+    Args:
+        grid: list descibing an island
+    Returns:
+        perimeter of described island
+    """
+
