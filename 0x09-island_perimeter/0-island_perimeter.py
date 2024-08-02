8394db9c1a62e7d6b2629f481e3419bf304c897e
arveyk <harveykisiangani@gmail.com>
2024-08-02 19:00:39 +0300

    if land is on the edges

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -21,14 +21,73 @@ def island_perimeter(grid):
         return 0
     for x in range(width):
         for y in range(length):
-            if grid[x][y] == 1:
-                ground.append((x, y))
-                if grid[x - 1][y] == 0:
-                    perimeter += 1
-                if grid[x][y - 1] == 0:
-                    perimeter += 1
-                if grid[x][y + 1] == 0:
-                    perimeter += 1
-                if grid[x + 1][y] == 0:
-                    perimeter += 1
+            if (x > 0 and x < width - 1) and (y > 0 and y < length - 1):
+                if grid[x][y] == 1:
+                    ground.append((x, y))
+                    if grid[x - 1][y] == 0:
+                        perimeter += 1
+                    if grid[x][y - 1] == 0:
+                        perimeter += 1
+                    if grid[x][y + 1] == 0:
+                        perimeter += 1
+                    if grid[x + 1][y] == 0:
+                        perimeter += 1
+            if x == 0:
+                if y == 0:
+                    if grid[x][y] == 1:
+                        if grid[x + 1][y] == 0:
+                            perimeter += 1
+                        if grid[x][y + 1] == 0:
+                            perimeter += 1
+                if y == length - 1:
+                    if grid[x][y] == 1:
+                        if grid[x + 1][y] == 0:
+                            perimeter += 1
+                        if grid[x][y - 1] == 0:
+                            perimeter += 1
+                if y > 0 and y < length - 1:
+                    if grid[x][y] == 1:
+                        if grid[x][y + 1] == 0:
+                            perimeter += 1
+                        if grid[x][y - 1] == 0:
+                            perimeter += 1
+                        if grid[x + 1][y] == 0:
+                            perimeter += 1
+            if x == width - 1:
+                if y == 0:
+                    if grid[x][y] == 1:
+                        if grid[x - 1][y] == 0:
+                            perimeter += 1
+                        if grid[x][y + 1] == 0:
+                            perimeter += 1
+                if y == length - 1:
+                    if grid[x][y] == 1:
+                        if grid[x - 1][y] == 0:
+                            perimeter += 1
+                        if grid[x][y - 1] == 0:
+                            perimeter += 1
+                if y > 0 and y < length - 1:
+                    if grid[x][y] == 1:
+                        if grid[x][y - 1] == 0:
+                            perimeter += 1
+                        if grid[x - 1][y] == 0:
+                            perimeter += 1
+                        if grid[x][y + 1] == 0:
+                            perimeter += 1
+                if x > 0 and x < width - 1:
+                    if y == 0:
+                        if grid[x - 1][y] == 0:
+                            perimeter += 1
+                        if grid[x][y + 1] == 0:
+                            perimeter += 1
+                        if grid[x + 1][y] == 0:
+                            perimeter += 1
+                    if y == length - 1:
+                        if grid[x - 1][y] == 0:
+                            perimeter += 1
+                        if grid[x][y - 1] == 0:
+                            perimeter += 1
+                        if grid[x + 1][y] == 0:
+                            perimeter += 1
+                        
     return perimeter
